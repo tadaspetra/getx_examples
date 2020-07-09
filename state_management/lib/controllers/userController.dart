e9fff80d8c0ae9b385b31529e24f8647712b4172
Amateur Coder <connectwithamateur@gmail.com>
2020-07-09 18:50:12 -0500

    ready for jonny to review

--- state_management/lib/controllers/userController.dart
+++ state_management/lib/controllers/userController.dart
@@ -5,6 +5,13 @@ class UserController extends GetxController {
   final user = User().obs;
 
   updateUser(int count) {
+    //Which is better??
+
+    //Option #1
+    user().name = "Tadas";
+    user().count = count;
+
+    //Option #2
     user.update((value) {
       value.name = 'Tadas';
       value.count = count;
